7616520c0fde708aea54e0430d80e50184b8106f
10youting01 <54501942+10youting01@users.noreply.github.com>
2025-04-01 17:31:38 +0800

    Update 238.product-of-array-except-self.py
    Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)

--- 238.product-of-array-except-self.py
+++ 238.product-of-array-except-self.py
@@ -1,13 +1,12 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        prefix = [1]*n
-        suffix = [1]*n
         ans = []
+        ans.append(1)
         for i in range(1, n):
-            prefix[i] = prefix[i-1]*nums[i-1]
-        for j in range(n-2, -1, -1):
-            suffix[j] = suffix[j+1]*nums[j+1]
-        for k in range(n):
-            ans.append(prefix[k]*suffix[k])
+            ans.append(ans[i-1]*nums[i-1])
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            ans[i] = ans[i]*suffix
+            suffix*=nums[i]
         return ans

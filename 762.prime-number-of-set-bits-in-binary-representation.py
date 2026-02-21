5a1af098808df1ab9e6afe92e8ebfca472fd2439
10youting01 <54501942+10youting01@users.noreply.github.com>
2026-02-21 18:44:05 +0100

    [Code] Add countPrimeSetBits method
    Refactor comments for clarity and update logic to count prime set bits in the binary representation of numbers.

--- 762.prime-number-of-set-bits-in-binary-representation.py
+++ 762.prime-number-of-set-bits-in-binary-representation.py
@@ -1,7 +1,20 @@
 class Solution:
     def countPrimeSetBits(self, left: int, right: int) -> int:
-        # turn each number in left to right to binary and store in an array of array
-        # count the set bits of each binary number array and store back
+        # turn each number in left to right to binary and store in an array of array -> bin
+        # count the set bits of each binary number array and store back -> count('1')
         # create a prime array store all the prime number smaller than 32(since int is at most 32 bits)
         # check and add one if the number od set bits in the array is in the prime array
         # return the number of prime 
+        arr = []
+        prime = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31}
+        for num in range(left, right+1):
+            arr.append(bin(num).count('1'))
+        
+        num_prim = 0
+        for idx, set_bit in enumerate(arr):
+            if set_bit in prime:
+                num_prim += 1
+        
+        return num_prim
+
+
